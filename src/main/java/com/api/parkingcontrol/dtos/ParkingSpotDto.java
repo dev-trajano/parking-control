ccf42e8ac4816e4df2c972f1e84805dcf80e6a9b
Joanderson Trajano <trajanojoanderson@gmail.com>
2022-08-23 23:04:06 -0300

    ParkingSpotMode ParkingSpotController

--- src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -1,46 +1,26 @@
-package com.api.parkingcontrol.models;
+package com.api.parkingcontrol.dtos;
 
-import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
-import javax.persistence.*;
-import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.UUID;
-
-@Entity
-@Table(name = "TB_PARKING_SPOT")
-public class ParkingSpotModel implements Serializable {
-  private static final long serialVersionUID = 1L;
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private UUID id;
-  @Column(nullable = false, unique = true, length = 10)
+public class ParkingSpotDto {
+  @NotBlank
   private String parkingSpotNumber;
-  @Column(nullable = false, unique = true, length = 7)
+  @NotBlank
+  @Size(max = 7)
   private String licensePlateCar;
-  @Column(nullable = false, length = 70)
+  @NotBlank
   private String brandCar;
-  @Column(nullable = false, length = 70)
+  @NotBlank
   private String modelCar;
-  @Column(nullable = false, length = 70)
+  @NotBlank
   private String colorCar;
-  @Column(nullable = false)
-  private LocalDateTime registrationDate;
-  @Column(nullable = false, length = 130)
+  @NotBlank
   private String responsibleName;
-  @Column(nullable = false, length = 30)
+  @NotBlank
   private String apartment;
-  @Column(nullable = false, length = 30)
+  @NotBlank
   private String block;
-
-  public UUID getId() {
-    return id;
-  }
-
-  public void setId(UUID id) {
-    this.id = id;
-  }
 
   public String getParkingSpotNumber() {
     return parkingSpotNumber;
@@ -80,14 +60,6 @@ public class ParkingSpotModel implements Serializable {
 
   public void setColorCar(String colorCar) {
     this.colorCar = colorCar;
-  }
-
-  public LocalDateTime getRegistrationDate() {
-    return registrationDate;
-  }
-
-  public void setRegistrationDate(LocalDateTime registrationDate) {
-    this.registrationDate = registrationDate;
   }
 
   public String getResponsibleName() {
